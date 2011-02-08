3ab5e7d3ad6b6007f6b93ac022d46cb30cb89147
jaakko.lukkari@gmail.com <jaakko.lukkari@gmail.com@3767fc57-7d02-cc52-5dda-a8c145f859eb>
2011-02-08 11:01:00 +0000

    git-svn-id: http://xruutu.googlecode.com/svn/trunk@2 3767fc57-7d02-cc52-5dda-a8c145f859eb

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+
+/* Copyright Jaakko Lukkari 2011 
+ *  
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+define( 'parentFile' , 1 );
+
+
+require_once('includes/variables.inc');
+
+
+
+$option="mainmenu";
+
+if(isset($_REQUEST['option'])) {
+	$option=$_REQUEST['option'];
+}
+
+
+
+
+if(file_exists($XRUUTUDIR.'components/'.$option.'/layout/default.php')
+&& file_exists($XRUUTUDIR.'components/'.$option.'/m.php')
+&& file_exists($XRUUTUDIR.'components/'.$option.'/v.php')
+&& file_exists($XRUUTUDIR.'components/'.$option.'/c.php')) {
+
+	require_once($XRUUTUDIR.'components/'.$option.'/m.php');
+	require_once($XRUUTUDIR.'components/'.$option.'/v.php');
+	require_once($XRUUTUDIR.'components/'.$option.'/c.php');
+
+	$layout=$XRUUTUDIR.'components/'.$option.'/layout';
+
+
+	//luodaan model
+	$modelclassname = $option."XRuutuModel";
+	$model = new $modelclassname($elisaviihde);
+
+	//luodaan view
+	$viewclassname = $option."XRuutuView";
+	$view = new $viewclassname();
+
+	//luodaan controlleri
+	$controllerclassname = $option."XRuutuController";
+	$contoller = new $controllerclassname($model, $view,$layout);
+
+
+	if(isset($_REQUEST['task'])) {
+
+		echo $contoller->execute($_REQUEST['task']);
+		
+	} else {
+
+		echo $contoller->render();
+
+	}
+
+} else {
+	echo "Komponenttia ".$option." ei löydy tai se on puutteellinen.";
+}
+
+
