2969fa22d7cf1db7b7f79ceca81cf5fb257dcffd
jaakko.lukkari@gmail.com <jaakko.lukkari@gmail.com@3767fc57-7d02-cc52-5dda-a8c145f859eb>
2011-02-09 19:37:44 +0000

    Lissätty enclousure
    git-svn-id: http://xruutu.googlecode.com/svn/trunk@15 3767fc57-7d02-cc52-5dda-a8c145f859eb

--- components/browse/layout/rss.item.php
+++ components/browse/layout/rss.item.php
@@ -2,4 +2,5 @@
 <title><![CDATA[<?php echo $item->title; ?>]]></title>
 <description><![CDATA[<?php echo $item->program_episode_description_text; ?>]]></description>
 <link>http://localhost:777/?r=rtmp://streamh1.nelonen.fi/hot/<?php $osat=explode(".",$item->video_filename); echo $osat[1]; ?>:<?php echo $item->video_filename; ?></link>
+<enclosure type="video/flv"  url="http://localhost:777/?r=rtmp://streamh1.nelonen.fi/hot/<?php $osat=explode(".",$item->video_filename); echo $osat[1]; ?>:<?php echo $item->video_filename; ?>" />
 </item>
