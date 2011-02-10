6bbb6397c940fcbf89f3857fd54127fd813bbc57
jaakko.lukkari@gmail.com <jaakko.lukkari@gmail.com@3767fc57-7d02-cc52-5dda-a8c145f859eb>
2011-02-10 16:07:38 +0000

    Lisätty uutisvideot lähde. Myöhemmin pitää tehdä paremmin.
    git-svn-id: http://xruutu.googlecode.com/svn/trunk@22 3767fc57-7d02-cc52-5dda-a8c145f859eb

--- components/browse/layout/rss.item.php
+++ components/browse/layout/rss.item.php
@@ -12,7 +12,12 @@ if($item->media_type=="video_episode"||$item->media_type=="video") {
 <description><![CDATA[<?php echo  $item->video_description_to_use; ?>]]></description>
 <shortdescription><![CDATA[<?php echo  $item->video_description_teaser; ?>]]></shortdescription>
 <?php  } ?>
-
+<?php if(endsWith($item->video_filename,"mp4")) { ?>
 <link>http://localhost:777/?r=rtmp://streamh1.nelonen.fi/hot/<?php $osat=explode(".",$item->video_filename); echo $osat[1]; ?>:<?php echo $item->video_filename; ?></link>
 <enclosure type="video/flv"  url="http://localhost:777/?r=rtmp://streamh1.nelonen.fi/hot/<?php $osat=explode(".",$item->video_filename); echo $osat[1]; ?>:<?php echo $item->video_filename; ?>" />
+<?php } else { ?>
+<link>http://uutisvideot.nelonen.fi/<?php echo $item->video_filename; ?></link>
+<enclosure type="video/flv"  url="http://uutisvideot.nelonen.fi/<?php echo $item->video_filename; ?>" />
+
+<?php } ?>
 </item>
